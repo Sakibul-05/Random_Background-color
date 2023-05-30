484d7e143d30d76e2a90db9554563ea70abf4d43
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-05-31 02:17:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+ const button = document.getElementById("btn");
+      function randomRgbColor() {
+        let red = parseInt(Math.random() * 256);
+        let green = parseInt(Math.random() * 256);
+        let blue = parseInt(Math.random() * 256);
+        return `rgb(${red},${green},${blue})`;
+      }
+      console.log(randomRgbColor());
+      function changeBackground() {
+        let rgb = randomRgbColor();
+        document.body.style.backgroundColor = rgb;
+        let showRgbValue = document.getElementById("showRgbValue");
+        showRgbValue.innerText = rgb;
+        showRgbValue.style.fontWeight = "bold";
+      }
